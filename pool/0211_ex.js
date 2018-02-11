8d1e0b209919614921bebc449ac977ffdf3c5646
Jay <fanzm1@lenovo.com>
2018-02-11 17:10:15 +0800

    5th commit, JSOOP solvedgit add pool!

--- pool/0211_ex.js
+++ pool/0211_ex.js
@@ -5,12 +5,23 @@
 
 
 
+
+
+
 //JSOOP(廖雪峰)探索验证...
 function ex_02112(){
+    /**
+     * 【正解区】
+     * 首先，do方法不是原型链上的。
+     * 
+     * 【解决一：将do绑定到Mmm.prototype（Mmm构造函数原型对象）上，对..就是Object，上！】
+     * 【解决二：将Mmm构造函数在Nnn构造函数里面重新跑一遍】
+     */
     /**
      * 想构造一个“5节点原型链”： n（实例）--Nnn.prototype（第二层）-- Mmm.prototype（第一层）-- Object.prototype -- null
      */
     //构造函数 Mmm(第一层)
+    //【正解区】：do确实是在Mmm(构造函数)上，但它没有在原型链上（也就是Mmm.prototype 原型对象上)
     function Mmm(){
         this.name = 'MMM';
         this.age = 18;
@@ -20,8 +31,13 @@ function ex_02112(){
     var mmm = new Mmm();
     mmm.do(); //输出 DO!! [第一层，没问题] 
 
+    //【1】将do绑定到Mmm.prototype（Mmm构造函数原型对象）上
+    // Mmm.prototype.do = function(){console.log("DO!!");}
+
     //构造函数 Nnn(第二层) 它的原型对象是Mmm
     function Nnn(){
+        //【2】将Mmm构造函数在Nnn构造函数里面重新跑一遍
+        Mmm.call(this);
         this.nnname = 'NNN';//仅增添一个属性
     }
 
@@ -34,14 +50,15 @@ function ex_02112(){
      * （虽然我到现在没搞懂 最后为什么要“修正”。。。）
      */
     function F(){}
-
     F.prototype = Mmm.prototype;
     Nnn.prototype = new F();
+    //以下 所谓的“修正”，没有必要！
+    //也就是说  >>>>  F空函数的加入/操作，没有对Nnn.prototype.constructor指向产生影响
     Nnn.prototype.constructor = Nnn;
 
-    //然鹅，并没有什么X用。
+    //然鹅，并没有什么X用。【。。。好了好了..】
     var nnn = new Nnn();
-    nnn.do();//报错！！。。。do方法没有继承过来...
+    nnn.do();//报错！！。。。do方法没有继承过来...【按照新方法，不会报错了！】
 }
 
 //廖雪峰 JSOOP 探索/验证
