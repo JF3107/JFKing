e2d14596d3e34813f60c677ac61af331e466daf6
Jay <fanzm1@lenovo.com>
2018-02-11 10:28:58 +0800

    0211 first push ..

--- 0204_JShot/0204_ex.js
+++ 0204_JShot/0204_ex.js
@@ -1,19 +1,22 @@
 
 
-function Person(name, age, job) {
-    this.name = name;
-    this.age = age;
-    this.job = job;
-    this.sayName = function() { alert(this.name) } 
+
+
+
+
+
+
+//闭包-未知
+function ex02111_unknow(){
+    function Person(name, age, job) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.sayName = function() { alert(this.name) } 
+    }
+    var person1 = new Person('Zaxlct', 28, 'Software Engineer');
+    var person2 = new Person('Mick', 23, 'Doctor');
 }
-var person1 = new Person('Zaxlct', 28, 'Software Engineer');
-var person2 = new Person('Mick', 23, 'Doctor');
-
-
-
-
-
-
 
 //闭包 example2
 function ex02042(){
